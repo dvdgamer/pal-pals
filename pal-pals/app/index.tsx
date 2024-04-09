076ae9b1964c7f1a9ad67e0003b490e10690ff98
dvdgamer <davidnmponte@gmail.com>
2024-04-09 18:33:20 +0200

    Application is broken Still can't seem to understand if I should use router or navigation

--- pal-pals/app/index.tsx
+++ pal-pals/app/index.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import { View, Text, ScrollView, SafeAreaView } from 'react-native';
-import { Stack, useRouter, useNavigation } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import Hello from "../components/Hello";
 import ScreenHeaderBtn from "../components/ScreenHeaderBtn";
-
 
 const Home = () => {
   // const router = useRouter();
@@ -11,25 +10,23 @@ const Home = () => {
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{
-            headerShadowVisible: false,
-            headerTitle: 'Home',
-            headerRight: () => (
-              <ScreenHeaderBtn title="Right Button" onClick={() => console.log('Button clicked')} />
-            ),
-            headerLeft: () => (
-              <ScreenHeaderBtn title="Left Button" onClick={() => console.log('Button clicked')} />
-            ),
-          }} />
-        <ScrollView>
-          <View>
-            <Text>Home Page</Text>
-            <Hello />
-          </View>
-        </ScrollView>
-      </Stack.Navigator>
+      <Stack.Screen
+        options={{
+          headerShadowVisible: false,
+          headerTitle: 'Home',
+          headerRight: () => (
+            <ScreenHeaderBtn title="Right Button" onClick={() => console.log('Button clicked')} />
+          ),
+          headerLeft: () => (
+            <ScreenHeaderBtn title="Left Button" onClick={() => console.log('Button clicked')} />
+          ),
+        }} />
+      <ScrollView>
+        <View>
+          <Text>Home Page</Text>
+          <Hello />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }

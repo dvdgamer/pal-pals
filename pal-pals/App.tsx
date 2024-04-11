f4cd971772f30d911b50a7585b3dafd26322af25
dvdgamer <davidnmponte@gmail.com>
2024-04-11 11:57:26 +0200

    Fixed Tailwinds type error, fully functional now Added Navigation file, now trying it

--- pal-pals/App.tsx
+++ pal-pals/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './app/index';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-
+import { NativeWindStyleSheet } from "nativewind";
 
 export default function App() {
   return (
@@ -10,6 +10,10 @@ export default function App() {
   );
 }
 
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 // const styles = StyleSheet.create({
 //   container: {

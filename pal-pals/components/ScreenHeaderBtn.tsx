56cbb790d85820e7e5de849f57478dd97a2235bf
dvdgamer <davidnmponte@gmail.com>
2024-04-12 13:36:10 +0200

    Settings button is now an icon Added style to Add friend button Add friend button is now TouchableOpacity

--- pal-pals/components/ScreenHeaderBtn.tsx
+++ pal-pals/components/ScreenHeaderBtn.tsx
@@ -1,17 +1,29 @@
-import { TouchableOpacity, Text } from "react-native";
+import { TouchableOpacity, Text, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 interface ScreenHeaderBtnProps {
   title: string;
   onClick: () => void;
+  iconUrl: any;
+  dimension: any;
 }
 
-const ScreenHeaderBtn: React.FC<ScreenHeaderBtnProps> = ({ title, onClick }) => {
-    const navigation = useNavigation();
+const ScreenHeaderBtn: React.FC<ScreenHeaderBtnProps> = ({
+  iconUrl,
+  dimension,
+  onClick,
+}) => {
+  const navigation = useNavigation();
 
   return (
     <TouchableOpacity onPress={onClick}>
-      <Text>{title}</Text>
+      <Image
+      source={iconUrl}
+      resizeMode="cover"
+      style={dimension}
+      className="my-4 mx-4"
+       />
+      {/* <Text>{title}</Text> */}
     </TouchableOpacity>
   );
 };

e6690425d3263ef301a35718c68816be8379d66f
dvdgamer <davidnmponte@gmail.com>
2024-04-19 18:42:29 +0200

    Added style to friendList Every name is now within a box Added a settings button to each name

--- pal-pals/app/screens/friendsList.tsx
+++ pal-pals/app/screens/friendsList.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
 interface Friend {
   id: string;
@@ -8,26 +15,53 @@ interface Friend {
 
 export default function FriendList() {
   const friends: Friend[] = [
-    { id: '1', name: 'John' },
-    { id: '2', name: 'Jane' },
-    { id: '3', name: 'Alice' },
-    { id: '4', name: 'Bob' },
+    { id: "1", name: "John" },
+    { id: "2", name: "Jane" },
+    { id: "3", name: "Alice" },
+    { id: "4", name: "Bob" },
   ];
 
   const renderFriend = ({ item }: { item: Friend }) => (
     <View>
-      <Text>{item.name}</Text>
+      <Text style={styles.friendElement}>{item.name}</Text>
+      <TouchableOpacity>
+        <Image
+          source={require("../../assets/images/cog.png")}
+          resizeMode="cover"
+          style={{ width: 30, height: 30 }}
+        />
+      </TouchableOpacity>
     </View>
   );
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text className='pt-5 mt-5'>Friend List</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text className="pt-5 mt-5">Friend List</Text>
       <FlatList
         data={friends}
         renderItem={renderFriend}
         keyExtractor={(item) => item.id}
+        style={styles.friendText}
       />
     </View>
-  )
-};
+  );
+}
+
+const styles = StyleSheet.create({
+  friendElement: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 24,
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+  },
+  friendText: {
+    fontSize: 24,
+    margin: 10,
+    width: "100%",
+    // paddingLeft: 10,
+  },
+});
